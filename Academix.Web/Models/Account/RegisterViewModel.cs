963f902582332599eb46d2a4b87837bcd5a77a10
VDanchev <viktordan315@gmail.com>
2024-03-26 18:55:09 +0200

    Added register view model

---
+++ Academix.Web/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Academix.Infrastructure.Constants;
+
+namespace Academix.Web.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression(ApplicationUserConstants.NameMatch, ErrorMessage = "First Name is invalid.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(ApplicationUserConstants.NameMatch, ErrorMessage = "First Name is invalid.")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email is invalid.")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
