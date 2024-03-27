cc7b94cfb092b7ed3e9d4c4e5e31502d69ec85e4
VDanchev <viktordan315@gmail.com>
2024-03-27 15:41:48 +0200

    Created new BaseController

---
+++ Academix.Web/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academix.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
