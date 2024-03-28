ffe0f59d544066416f1d008932a73f0b52823082
VDanchev <viktordan315@gmail.com>
2024-03-28 19:59:03 +0200

    Added new entity

---
+++ Academix.Infrastructure/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academix.Infrastructure.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            Subjects = new List<Subject>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public IdentityUser User { get; set; } = null!;
+
+        [Required]
+        public int SchoolId { get; set; }
+
+        [ForeignKey(nameof(SchoolId))]
+        public School School { get; set; } = null!;
+
+        [Required]
+        public int ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]
+        public Parent Parent { get; set; } = null!;
+
+        public ICollection<Subject> Subjects { get; set; }
+    }
+}
