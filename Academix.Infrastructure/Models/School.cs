1ad250648028b64ced609288ad9eb95a7a6f2967
VDanchev <viktordan315@gmail.com>
2024-03-28 19:58:58 +0200

    Added new entity

---
+++ Academix.Infrastructure/Models/School.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academix.Infrastructure.Models
+{
+    public class School
+    {
+        public School()
+        {
+            Teachers = new List<Teacher>();
+            Students = new List<Student>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int CityId { get; set; }
+
+        [ForeignKey(nameof(CityId))]
+        public City City { get; set; } = null!;
+
+        public ICollection<Teacher> Teachers { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
