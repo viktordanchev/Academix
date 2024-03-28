f46a920555df247c97e54d3e9167c025fa21cd11
VDanchev <viktordan315@gmail.com>
2024-03-28 19:58:53 +0200

    Added new entity

---
+++ Academix.Infrastructure/Models/Parent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Academix.Infrastructure.Models
+{
+    public class Parent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public IdentityUser User { get; set; } = null!;
+
+
+        public int MyProperty { get; set; }
+    }
+}
