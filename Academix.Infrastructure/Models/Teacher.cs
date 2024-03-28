1dc99db678f197567e800cdb1c16ae1b7c76003f
VDanchev <viktordan315@gmail.com>
2024-03-28 19:59:08 +0200

    Added new entity

---
+++ Academix.Infrastructure/Models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academix.Infrastructure.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public IdentityUser User { get; set; } = null!;
+
+        public int SchoolId { get; set; }
+
+        [ForeignKey(nameof(SchoolId))]
+        public School School { get; set; } = null!;
+
+        public string? Address { get; set; }
+    }
+}
