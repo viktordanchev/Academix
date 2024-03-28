df626407993151fe1ed87e353f18088ee793bde0
VDanchev <viktordan315@gmail.com>
2024-03-28 19:58:27 +0200

    Added new entity

---
+++ Academix.Infrastructure/Models/Absence.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Academix.Infrastructure.Models.Enums;
+
+namespace Academix.Infrastructure.Models
+{
+    public class Absence
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateAndTime { get; set; }
+
+        [Required]
+        public AbsenceTypes AbsenceType { get; set; }
+    }
+}
