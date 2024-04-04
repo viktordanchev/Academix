b7d7cb89344f8dd6aea80df8660dc0f927e7b693
VDanchev <viktordan315@gmail.com>
2024-04-04 20:38:21 +0300

    Added new models

---
+++ Academix.Web/Models/Account/InfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academix.Web.Models.Account
+{
+    public class InfoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Academix.Web/Models/Account/SignUpViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Academix.Infrastructure.Constants;
+
+namespace Academix.Web.Models.Account
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+            Roles = new List<IdentityRoleViewModel>();
+            Schools = new List<InfoViewModel>();
+            Students = new List<InfoViewModel>();
+            Classes = new List<InfoViewModel>();
+        }
+
+        [Required(ErrorMessage = "The First name field is required.")]
+        [RegularExpression(ApplicationUserConstants.NameMatch, ErrorMessage = "First name is invalid.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Last name field is required.")]
+        [RegularExpression(ApplicationUserConstants.NameMatch, ErrorMessage = "First name is invalid.")]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "Email is invalid.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Password field is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Role field is required.")]
+        public string IdentityRoleId { get; set; } = null!;
+
+        [Required(ErrorMessage = "The School field is required.")]
+        public int SchoolId { get; set; }
+
+        [Required(ErrorMessage = "The Student field is required.")]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "The Class field is required.")]
+        public int ClassId { get; set; }
+
+        public IEnumerable<IdentityRoleViewModel> Roles { get; set; }
+
+        public IEnumerable<InfoViewModel> Schools { get; set; }
+
+        public IEnumerable<InfoViewModel> Students { get; set; }
+
+        public IEnumerable<InfoViewModel> Classes { get; set; }
+    }
+}
