6bb3b27f20274590a970bf3a586397d699f704bb
VDanchev <viktordan315@gmail.com>
2024-04-10 20:37:15 +0300

    Added new director area

---
+++ Academix.Web/Areas/Director/Controllers/DirectorBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Academix.Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academix.Web.Areas.Director.Controllers
+{
+    [Area(RoleConstants.Director.RoleName)]
+    [Authorize(Roles = RoleConstants.Director.RoleName)]
+    public class DirectorBaseController : Controller
+    {
+    }
+}
---
+++ Academix.Web/Areas/Director/Models/Request/AllViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academix.Web.Areas.Director.Models.Request
+{
+    public class AllViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Role { get; set; } = null!;
+
+        [Required]
+        public string RequesterName { get; set; } = null!;
+
+        public string? Message { get; set; }
+    }
+}
