e8162261185992bb37eb21e12808239efc2d16da
VDanchev <viktordan315@gmail.com>
2024-04-14 17:50:21 +0300

    Added new service

---
+++ Academix.Core/Contracts/IRequestService.cs
@@ -0,0 +1,10 @@
+﻿using Academix.Core.Models.Request;
+
+namespace Academix.Core.Contracts
+{
+    public interface IRequestService
+    {
+        Task<IEnumerable<AllViewModel>> GetAllRequestsToAdmin(string userId);
+        Task<IEnumerable<AllViewModel>> GetAllRequestsToDirector(string userId);
+    }
+}
