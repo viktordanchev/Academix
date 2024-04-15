00c3b9191b985e1b107fa53ace30af5a990d10f5
VDanchev <viktordan315@gmail.com>
2024-04-15 20:57:47 +0300

    Added interfaces

--- Academix.Core/Contracts/IRequestService.cs
+++ Academix.Core/Contracts/IRequestService.cs
@@ -1,4 +1,5 @@
 ﻿using Academix.Core.Models.Request;
+using Academix.Infrastructure.Data.Models;
 
 namespace Academix.Core.Contracts
 {
@@ -6,5 +7,8 @@ namespace Academix.Core.Contracts
     {
         Task<IEnumerable<AllViewModel>> GetAllRequestsToAdmin(string userId);
         Task<IEnumerable<AllViewModel>> GetAllRequestsToDirector(string userId);
+        Task<RequestServiceModel> GetRequestById(int requestId);
+        Task AssignToRole(RequestServiceModel request);
+        Task RemoveRequest(int requestId);
     }
 }
---
+++ Academix.Core/Contracts/IStudentHomeService.cs
@@ -0,0 +1,10 @@
+﻿using Academix.Core.Models.StudentHome;
+
+namespace Academix.Core.Contracts
+{
+    public interface IStudentHomeService
+    {
+        Task<int> GetStudent(string studentId);
+        Task<IEnumerable<SubjectViewModel>> GetAllSubjects(int studentId);
+    }
+}
