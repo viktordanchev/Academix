a787a5420394667f99a2e28fc51aa38aec3b4c63
VDanchev <viktordan315@gmail.com>
2024-04-15 20:58:44 +0300

    Updated area views

---
+++ Academix.Web/Areas/Student/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Academix.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Academix.Web.Areas.Student.Controllers
+{
+    public class HomeController : StudentBaseController
+    {
+        private readonly IStudentHomeService _studentHomeService;
+
+        public HomeController(IStudentHomeService studentHomeService)
+        {
+            _studentHomeService = studentHomeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var student = await _studentHomeService.GetStudent(GetUserId());
+            var subjects = await _studentHomeService.GetAllSubjects(student);
+
+            return View(subjects);
+        }
+
+        private string GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
---
+++ Academix.Web/Areas/Student/Controllers/StudentBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Academix.Common.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academix.Web.Areas.Student.Controllers
+{
+    [Area(RoleConstants.Student.RoleName)]
+    [Authorize(Roles = RoleConstants.Student.RoleName)]
+    public class StudentBaseController : Controller
+    {
+    }
+}
