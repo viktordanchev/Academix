0235902943e22791e4d2448b2d972e8834ebe234
VDanchev <viktordan315@gmail.com>
2024-04-20 21:04:13 +0300

    Removed

--- Academix.Common/Constants/ClassConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Academix.Common.Constants
-{
-    public static class ClassConstants
-    {
-        public const int ClassNumberMin = 1;
-        public const int ClassNumberMax = 35;
-    }
-}
