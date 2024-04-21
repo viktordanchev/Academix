358638f9e1a1185a34e4b6e89fc5d8c472e198c6
VDanchev <viktordan315@gmail.com>
2024-04-21 11:31:51 +0300

    Removed methods

--- Academix.Core/Contracts/IStudentService.cs
+++ Academix.Core/Contracts/IStudentService.cs
@@ -4,8 +4,6 @@ namespace Academix.Core.Contracts
 {
     public interface IStudentService
     {
-        Task<IEnumerable<InfoViewModel>> GetClassesBySchoolAsync(int schoolId);
-        Task<IEnumerable<InfoViewModel>> GetStudentsByClassAsync(int classId);
         Task<StudentViewModel> GetStudentAsync(int studentId);
         Task<int> GetSchoolIdAsync(string teacherId);
     }
