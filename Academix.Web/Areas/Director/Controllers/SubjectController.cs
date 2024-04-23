c6ee05fd6085607dcb9b7307119253214e122d02
VDanchev <viktordan315@gmail.com>
2024-04-23 19:41:06 +0300

    Added controller and views

---
+++ Academix.Web/Areas/Director/Controllers/SubjectController.cs
@@ -0,0 +1,97 @@
+﻿using Academix.Core.Contracts;
+using Academix.Core.Models.Subject;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academix.Web.Areas.Director.Controllers
+{
+    public class SubjectController : DirectorBaseController
+    {
+        private readonly ISubjectService _subjectService;
+
+        public SubjectController(ISubjectService subjectService)
+        {
+            _subjectService = subjectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var schoolId = await _subjectService.GetSchoolIdAsync(GetUserId());
+            var subjects = await _subjectService.GetSubjectsAsync(schoolId);
+
+            return View(subjects);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var schoolId = await _subjectService.GetSchoolIdAsync(GetUserId());
+            var teachers = await _subjectService.GetTeachersAsync(schoolId);
+
+            var model = new AddSubjectViewModel();
+            model.Teachers = teachers;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddSubjectViewModel model)
+        {
+            var schoolId = await _subjectService.GetSchoolIdAsync(GetUserId());
+
+            if(!await IsModelValid(schoolId, model))
+            {
+                var teachers = await _subjectService.GetTeachersAsync(schoolId);
+                model.Teachers = teachers;
+
+                return View(model);
+            }
+
+            await _subjectService.AddSubjectAsync(schoolId, model);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove(int id)
+        {
+            if(await _subjectService.IsSubjectExistAsync(id))
+            {
+                await _subjectService.RemoveSubjectAsync(id);
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public async Task<IActionResult> AddStudent(int id)
+        {
+            if (await _subjectService.IsSubjectExistAsync(id))
+            {
+                await _subjectService.RemoveSubjectAsync(id);
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        private async Task<bool> IsModelValid(int schoolId, AddSubjectViewModel model)
+        {
+            var isValid = true;
+
+            if (await _subjectService.IsTeacherHasSubject(model.TeacherId))
+            {
+                ModelState.AddModelError(string.Empty, "This teacher has subject already");
+
+                isValid = false;
+            }
+
+            if (!await _subjectService.IsTeacherExistInSchool(schoolId, model.TeacherId))
+            {
+                ModelState.AddModelError(string.Empty, "This teacher doesn't exist");
+
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
